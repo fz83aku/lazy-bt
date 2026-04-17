5b750fcdad5b1bcb403b5c9d430a4489d8c2c637
fz83aku <zapf.fdez@gmail.com>
2026-04-17 11:40:13 +0200

    Update lazy-bt.js

--- lazy-bt.js
+++ lazy-bt.js
@@ -1,20 +1,19 @@
-export default class SlowLazyScroll {
+class SlowLazyScroll {
   constructor() {
     this.name = "slow-lazy-scroll";
   }
 
   async init(page, context) {
-    // kurze Start-Wartezeit, damit erste Inhalte laden
+    // kurze Start-Wartezeit
     await page.waitForTimeout(3000);
 
     let previousHeight = 0;
     let currentHeight = await page.evaluate(() => document.body.scrollHeight);
 
-    // solange scrollen, bis sich die Seitenhöhe nicht mehr verändert
+    // solange scrollen, bis keine neue Höhe mehr entsteht
     while (currentHeight !== previousHeight) {
       previousHeight = currentHeight;
 
-      // langsam nach unten scrollen in kleinen Schritten
       await page.evaluate(async () => {
         await new Promise((resolve) => {
           let totalHeight = 0;
@@ -28,17 +27,15 @@ export default class SlowLazyScroll {
               clearInterval(timer);
               resolve();
             }
-          }, 500); // langsames Scroll-Intervall
+          }, 500);
         });
       });
 
-      // warten, damit Lazy Content nachladen kann
       await page.waitForTimeout(2000);
 
       currentHeight = await page.evaluate(() => document.body.scrollHeight);
     }
 
-    // am Ende kurz ganz nach unten sichern
     await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
     await page.waitForTimeout(2000);
   }
